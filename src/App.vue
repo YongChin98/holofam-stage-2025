9338c0af54191dfdbee5603d7bb1d83e060eaa50
Yong Chin Ong <yongchin1910@gmail.com>
2025-04-30 19:10:16 +0800

    Revert "-initial commit"
    This reverts commit aaf9ba68b9d5826e7d783bd44d9941ce9492a484.

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <transition name="fade">
+      <SplashScreen v-if="showSplash" :gifSrc="`/images/background/hololive-sync-splash.gif?t=${Date.now()}`" />
+    </transition>
+      <router-view  />
+  </div>
+</template>
+
+<script setup>
+import SplashScreen from './components/Splashscreen.vue'
+import { ref, onMounted } from 'vue'
+
+const showSplash = ref(true)
+
+onMounted(() => {
+  setTimeout(() => {
+    showSplash.value = false
+  }, 2500) // 1 second = 1000ms
+})
+</script>
+
+<style scoped>
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.4s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ src/components/Splashscreen.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="splash-screen">
+      <img :src="gifSrc" alt="Loading..." />
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineProps } from 'vue'
+  
+  // Accept props if you want to pass a custom gif
+  defineProps({
+    gifSrc: {
+      type: String,
+      default: '/splash.gif' // default gif path
+    }
+  })
+  </script>
+  
+  <style scoped>
+  .splash-screen {
+    position: fixed;
+    inset: 0;
+    background: #000; /* You can change background color */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 9999; /* Stay on top */
+    transition: opacity 0.4s ease;
+  }
+  
+  img {
+    height: auto;
+  }
+  </style>
+  
